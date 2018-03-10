4ff645feec936b56082bc3c9b4c0951e1f27f612
Albert De Nova <albert.dnova@gmail.com>
2018-03-10 20:49:58 +0100

    Add GsonArrayRequest implementation

--- app/src/main/java/com/lasalle/mdpa/busybudgeter/network/impl/BudgetRestApiVolley.java
+++ app/src/main/java/com/lasalle/mdpa/busybudgeter/network/impl/BudgetRestApiVolley.java
@@ -1,20 +1,13 @@
 package com.lasalle.mdpa.busybudgeter.network.impl;
 
-import android.arch.lifecycle.LiveData;
-import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.Observer;
 import android.util.Log;
 
-import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.toolbox.JsonArrayRequest;
 import com.lasalle.mdpa.busybudgeter.database.entity.Budget;
 import com.lasalle.mdpa.busybudgeter.network.BudgetRestAPI;
+import com.lasalle.mdpa.busybudgeter.network.util.GsonArrayRequest;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -27,38 +20,12 @@ public class BudgetRestApiVolley implements BudgetRestAPI {
 
     public void fetchList(Observer<List<Budget>> budgetListObserver)
     {
-        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest
-                (Request.Method.GET, FETCH_URL, null, successResponse -> {
-                    List<Budget> parsedList = parseBudgetList(successResponse);
-                    budgetListObserver.onChanged(parsedList);
-                }, errorResponse -> {
-                    // TODO: Handle error
-                    Log.d("Async", "Error!!");
-                });
-
-        requestQueue.add(jsonArrayRequest);
-    }
-
-    private List<Budget> parseBudgetList(JSONArray jsonArray)
-    {
-        List<Budget> budgetList = new ArrayList<>();
-
-        try {
-            for(int i = 0; i < jsonArray.length(); ++i)
-            {
-                JSONObject jsonObject = jsonArray.getJSONObject(i);
-
-                Budget budget = new Budget();
-                budget.setId(jsonObject.getInt("id"));
-                budget.setName(jsonObject.getString("name"));
-
-                budgetList.add(budget);
-            }
-        }
-        catch(Exception e) {
-            e.printStackTrace();
-        }
-
-        return budgetList;
+        GsonArrayRequest<Budget> gsonRequest = new GsonArrayRequest<>(
+            FETCH_URL, Budget.class, null, response -> {
+                budgetListObserver.onChanged(response);
+            }, error -> {
+                Log.d("Async", "Error!!");
+            });
+        requestQueue.add(gsonRequest);
     }
 }
