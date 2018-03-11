a4c4263e235956e2209efdd29d9af544ebd1a688
Albert De Nova <albert.dnova@gmail.com>
2018-03-11 20:41:06 +0100

    Implement okHttp library

--- app/src/main/java/com/lasalle/mdpa/busybudgeter/BudgetingApplication.java
+++ app/src/main/java/com/lasalle/mdpa/busybudgeter/BudgetingApplication.java
@@ -5,14 +5,17 @@ import android.arch.persistence.room.Room;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
 import com.lasalle.mdpa.busybudgeter.database.BudgetingDatabase;
 import com.lasalle.mdpa.busybudgeter.manager.BudgetManager;
 import com.lasalle.mdpa.busybudgeter.manager.UserManager;
 import com.lasalle.mdpa.busybudgeter.manager.impl.BudgetManagerImpl;
 import com.lasalle.mdpa.busybudgeter.manager.impl.UserManagerImpl;
 import com.lasalle.mdpa.busybudgeter.network.BudgetRestAPI;
+import com.lasalle.mdpa.busybudgeter.network.impl.BudgetRestApiOkHttp;
 import com.lasalle.mdpa.busybudgeter.network.impl.BudgetRestApiVolley;
 
+import okhttp3.OkHttpClient;
 import toothpick.Scope;
 import toothpick.Toothpick;
 import toothpick.config.Module;
@@ -34,8 +37,10 @@ public class BudgetingApplication extends Application {
             bind(UserManager.class).to(UserManagerImpl.class);
             bind(BudgetingDatabase.class).toInstance(createBudgetingDatabaseInstance());
             bind(RequestQueue.class).toInstance(Volley.newRequestQueue(application));
-            bind(BudgetRestAPI.class).to(BudgetRestApiVolley.class);
+            bind(BudgetRestAPI.class).to(BudgetRestApiOkHttp.class);
             bind(BudgetManager.class).to(BudgetManagerImpl.class);
+            bind(OkHttpClient.class).toInstance(new OkHttpClient());
+            bind(Gson.class).toInstance(new Gson());
         }});
     }
 
